ed38a478f27eb4b03f461f7610985ea9b8d4715d
Richard Harris <richardharris916@gmail.com>
2025-11-17 00:05:56 -0600

    Added not supported banner Documentation

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-alert("SCRIPT LOADED");
+// Grab elements
 /** @type {HTMLInputElement} */
+const container = document.querySelector(".container");
 const fileInput = document.querySelector("#fileInput");
 const preview = document.querySelector("#preview");
 const previewLabel = document.querySelector("#preview label");
 const colors = document.querySelectorAll("#theme .color");
 const colorValue = document.querySelector("#value");
 
-alert("fileInput found: " + (fileInput !== null));
-alert("fileInput id: " + fileInput?.id);
+// Initialize properties
 /** @type {File} */
 let file = null;
 /** @type {EyeDropper} */
 let eyeDropper = null;
+
+// Show alert if not supported
 try {
   eyeDropper = new EyeDropper();
 } catch (e) {
-  alert("Eyedropper not supported");
+  fileInput.style.display = "none";
+  const banner = document.createElement("div");
+  banner.id = "notSupported";
+  container.prepend(banner);
 }
+
+// Add click/touch events for each color selector
 colors.forEach((color) => {
   let startTime;
 
+  // If a file is selected, initiate eye dropper color selection
   color.addEventListener("click", () => {
     if (file !== null) {
       eyeDropper.open().then((colorResult) => {
@@ -29,17 +37,23 @@ colors.forEach((color) => {
     }
   });
 
+  // If a background color is already selected, display its hex value
   color.addEventListener("mouseover", () => {
     if (color.style.backgroundColor) colorValue.innerHTML = color.dataset.hex;
   });
+
+  // Clear the innerHTML and start a timer
   color.addEventListener("touchstart", () => {
     colorValue.innerHTML = "";
     startTime = Date.now();
   });
 
+  // Clear the innerHTML
   color.addEventListener("mouseleave", () => {
     colorValue.innerHTML = "";
   });
+
+  // If the time between touchstart and touchend is more than 200 milliseconds and a background color is already selected, display its hex value
   color.addEventListener("touchend", (e) => {
     const duration = Date.now() - startTime;
 
@@ -50,24 +64,17 @@ colors.forEach((color) => {
   });
 });
 
-try {
-  fileInput.addEventListener("change", () => alert("CHANGE Listener fired"));
-  fileInput.addEventListener("click", () => alert("CLICK Listener fired"));
-  fileInput.addEventListener("focus", () => alert("FOCUS Listener fired"));
-  fileInput.addEventListener("blur", () => alert("BLUR Listener fired"));
-  if (fileInput) alert("Adding listener");
-  fileInput.addEventListener("input", () => {
-    setTimeout(() => {
-      alert("INPUT Listener fired");
-      alert(`files ${fileInput.files?.[0]?.name}`);
-      alert(`value ${fileInput.value}`);
-      if (fileInput.files?.length || fileInput.value) {
+// If a file is selected, display the image
+fileInput.addEventListener("input", () => {
+  setTimeout(() => {
+    try {
+      if (fileInput.files?.length) {
         const existingImage = document.querySelector("#previewImage");
         const existingFileName = document.querySelector("#fileName");
         if (existingImage) preview.removeChild(existingImage);
         if (existingFileName) preview.removeChild(existingFileName);
         colors.forEach((color) => (color.style.backgroundColor = ""));
-        file = fileInput.files?.[0] || fileInput.value;
+        file = fileInput.files[0];
 
         const image = document.createElement("img");
         image.id = "previewImage";
@@ -79,9 +86,8 @@ try {
         preview.prepend(fileName);
         preview.prepend(image);
       }
-    }, 10);
-  });
-} catch (e) {
-  alert(`ERROR: ${e.message}`);
-}
-alert("Added listener");
+    } catch (e) {
+      alert(`ERROR: ${e.message}`);
+    }
+  }, 10);
+});
